2e18509777753eafa372089d90b2b87d3fb23aa7
Tim Atherton <taweb@github.com>
2018-06-13 00:58:34 +0100

    basic slider to change autoregeneration rate

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -7,7 +7,10 @@ class Controls extends Component {
 		this.setAutoGeneration = this.setAutoGeneration.bind(this);
 		this.toggleAutoGeneration = this.toggleAutoGeneration.bind(this);
 		this.randomise = this.randomise.bind(this);
-
+		this.onRateChange = this.onRateChange.bind(this);
+		this.state = {
+			rate: 10
+		};
 	} 
 
 	onClick(){
@@ -18,15 +21,16 @@ class Controls extends Component {
 			this.props.nextGeneration();
 	}	
 
-	toggleAutoGeneration(){
+	toggleAutoGeneration(rate){
 		const { auto } = this.props;
+		const updateRate = false;
 		this.props.toggleAutoGeneration();
-		this.setAutoGeneration(auto);
+		this.setAutoGeneration(updateRate, auto, rate);
 	}
 
-	setAutoGeneration(setting) {
-		!setting ? 
-			this.update = setInterval(this.props.nextGeneration, 100)
+	setAutoGeneration(updateRate, setting, rate) {
+		!setting || updateRate ? 
+			this.update = setInterval(this.props.nextGeneration, rate)
 			:
 			clearInterval(this.update);
 	}
@@ -36,14 +40,43 @@ class Controls extends Component {
 		this.props.randomise(factor);
 	}
 
+	onRateChange(e) {
+		const { value } = e.target;
+		this.setState({
+			rate: value
+		}, this.UpdateAutoGeneration(value))
+	}
+
+	UpdateAutoGeneration(value){
+	// 	const adjustedRate = 1000 - value;
+	// 	clearInterval(this.update);
+	// 	this.update = setInterval(adjustedRate); 
+	clearInterval(this.update);
+	const { auto } = this.props;
+	const updateRate = true;
+	const adjustedRate = 860 - value
+	this.setAutoGeneration(updateRate, auto, adjustedRate)
+	}
+
 	render() {
 		const { auto } = this.props;
+		const { rate } = this.state;
+		const adjustedRate = 860 - rate;
 		return (
 			<div> 
 				<button onClick={this.onClick}>Next Generation</button>
-				<button onClick={this.toggleAutoGeneration}>
+				<button onClick={()=>this.toggleAutoGeneration(adjustedRate)}>
 					{!auto ? "start" : "stop"}
 				</button>
+				<input 
+					type="range" 
+					min="0" 
+					max="800" 
+					value= {this.state.rate} 
+					id="utilslider"
+					onChange={this.onRateChange}
+				/>
+				<p>{this.state.rate}</p>
 				<button onClick={() => this.randomise(0.5)}>Randomise!</button>
 			</div>
 		)
