72fc534ce502a0bb98e38cd4b1af59d72d836261
Tim Atherton <taweb@github.com>
2019-05-22 17:40:04 +0100

    update to create-react-app 2.0

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,41 +1,38 @@
-import React from 'react';
-import { Rect } from 'react-konva';
+import React, {Component} from 'react';
 
-const Cell = (props) => {
-	this.handleClick = () => {
-		const { id, gridId } = props;
-		props.onClick(id, gridId);
+class Cell extends Component {
+	constructor(props){
+		super(props)
+		this.handleClick = this.handleClick.bind(this)
 	}
 
-	const { id, currentCell, xDim, cellSize } = props;
-	const row = Math.floor(id/xDim);
-	const col = id % xDim;
-	const left = col * cellSize;
-	const top = row * cellSize;
-	
-	return (
-		// <div 
-		// 	className={["cell", isLive].join(' ')}
-		// 	onClick={this.handleClick}
-		// 	style={{
-		// 		position: "absolute",
-		// 		left: `${left}px`,
-		// 		top: `${top}px`,
-		// 		width: `${cellSize}px`,
-		// 		height: `${cellSize}px`
-		// 	}}
-		// />
-		<Rect
-			x={left}
-			y={top}
-			width={cellSize}
-			height={cellSize}
-			fill={currentCell ? '#3de045' : '#414284'}
-			stroke={'#d1d1d1'}
-			strokeWidth={1}
-			onClick={this.handleClick}
-		/>
-	)
+	handleClick() {
+		const { id, gridId, onClick } = this.props;
+		onClick(id, gridId);
+	}
+
+	render() {
+		const { id, currentCell, xDim, cellSize } = this.props;
+		const row = Math.floor(id/xDim);
+		const col = id % xDim;
+		const left = col * cellSize;
+		const top = row * cellSize;
+		const isLive = currentCell ? 'living' : ''
+
+		return (
+			<div 
+				className={["cell", isLive].join(' ')}
+				onClick={this.handleClick}
+				style={{
+					position: "absolute",
+					left: `${left}px`,
+					top: `${top}px`,
+					width: `${cellSize}px`,
+					height: `${cellSize}px`
+				}}
+			/>
+		)
+	}
 }
 
 export default Cell;
